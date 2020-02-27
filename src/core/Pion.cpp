a098f68bcb950e7d1318eb9ec5a5a8b787f246ac
Laetitia <laetitia.laporte@etu.univ-lyon1.fr>
2020-02-27 16:23:48 +0100

    Class Pion
    Début de la classe pion

--- src/core/Pion.cpp
+++ src/core/Pion.cpp
@@ -1,1 +1,53 @@
 #include "Pion.h"
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+//Constructeur par défaut
+Pion::Pion(){
+
+	x = 0;
+	karma = 0;
+	bitcoin = INITCOIN;
+	for (int i = 0; i < MAXCASEHT; i++)
+		{
+			IndCaseAchetee[i] = -1;
+		}
+	c = '*';
+
+}
+
+Pion::Pion(){
+
+
+}
+
+//Accesseurs
+int Pion::getpos()const{ return pos;}
+int Pion::getkarma()const{ return karma;}
+float Pion::getbitcoin()const{ return bitcoin;}
+int Pion::getIndCaseAchetee(){
+
+//A faire
+
+}
+
+//Mutateurs
+void Pion::setpos(const float pos1){ pos = pos1;}
+void Pion::setkarma(){ /*A faire*/ }
+
+//Fonction
+int Avancer(int pos)
+{
+	srand (time(NULL));      		//Initialisation de la fonction rand
+	int d1 = rand() % 6 + 1; 		//entier aléatoire entre 1 et 6 
+	int d2 = rand() % 6 + 1;
+	pos = pos + d1 + d2;
+	if(pos > MAXCASEP)
+	{
+		pos = pos - MAXCASEP - 1
+	}
+	
+	return pos;
+}
+
+//Destructeur
--- src/core/Pion.h
+++ src/core/Pion.h
@@ -1,11 +1,41 @@
 #ifndef OCTET_PION_H
 #define OCTET_PION_H
 
+const int MAXCASEHT = 24; //Max case achetée
+const int MAXCASEP = 32; //Max case du plateau
+const int INITCOIN = 0; //A changer
+
 class Pion{
-	private :
+	private :	int pos;								//Position du pion
+				int karma;								//Karma positif ou négatif du pion
+				float bitcoin;							//Argent du pion
+				int IndCaseAchetee[MAXCASEHT];			//Indice des cases achetées
+				char c ; 								//Caractère du pion
+
+	public :	
+				//Constructeur par défaut
+				Pion();		
+
+				//Accesseurs	
+				int getpos();
+				int getkarma();
+				float getbitcoin();
+				int getIndCaseAchetee();
+
+				//Mutateurs
+				void setpos(x1);
+				void setkarma();
+				void setbitcoin();
+				void setIndCaseAchetee();
+				void setc();
+
+				//Fonctions
+				int Avancer(pos);
+
+
+				//Destructeur
+				~ Pion();
 
 };
-
-
 
 #endif
