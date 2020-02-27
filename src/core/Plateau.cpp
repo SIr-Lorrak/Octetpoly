3561a150c4dd06173be6d6c55fbd4d9854e1e647
Cogoni <guillaume.cogoni@etu.univ-lyon1.fr>
2020-02-27 14:43:13 +0100

    Begin class Plateau

--- src/core/Plateau.cpp
+++ src/core/Plateau.cpp
@@ -1,3 +1,52 @@
 #include "Plateau.h"
 
+using namespace std;
+
+Plateau::Plateau(){
+	nbcase = 32;
+	//tab	= new Case[taille];
+
+	nbJoueur = 0;
+	nbOrdi = 0;
+	/*
+	j1 = new Joueur;
+	j2 = new Joueur;
+	j3 = new Joueur;
+	j4 = new Joueur;
+	*/
+	/*
+	ordi1 =  new Ordi;
+	ordi2 =  new Ordi;
+	ordi3 =  new Ordi;
+	ordi4 =  new Ordi;
+	*/
+
+	dimx = 0;
+	dimy = 0;
+}
+
+
+Plateau::Plateau(const unsigned int dimensionX, const unsigned int dimensionY, const unsigned int nombreDeJoueur){
+
+	nbcase = 32;
+	//tab	= new Case[taille];
+
+	nbJoueur = nbJoueur;
+	nbOrdi = 4 - nbJoueur;
+	/*
+	j1 = new Joueur;
+	j2 = new Joueur;
+	j3 = new Joueur;
+	j4 = new Joueur;
+
+	ordi1 =  new Ordi;
+	ordi2 =  new Ordi;
+	ordi3 =  new Ordi;
+	ordi4 =  new Ordi;
+	*/
+
+	dimx = dimensionX;
+	dimy = dimensionY;
+}
+
 
--- src/core/Plateau.h
+++ src/core/Plateau.h
@@ -4,8 +4,83 @@
 #include "Case.h"
 #include "Joueur.h"
 #include "Ordi.h"
+//#include <SDL_image.h>
 
 class Plateau{
+private:
+
+	//Champs liée au plateau
+	 	unsigned int nbcase;
+	 	Case * tab;
+
+	 	unsigned int nbJoueur;
+	 	unsigned int nbOrdi;
+	 	/*
+	 	Joueur j1;
+	 	Joueur j2;
+	 	Joueur j3;
+	 	Joueur j4;
+		*/
+		/*
+	 	Ordi ordi1;
+	 	Ordi ordi2;
+	 	Ordi ordi3;
+	 	Ordi ordi4;
+		*/
+		
+	 //Champs liée à la SDL
+	 	unsigned int dimx, dimy;
+	 	/*
+		SDL_Window * window;
+		SDL_Renderer * renderer;
+		SDL_Surface* surface;
+		SDL_Texture* texture;
+		int x;
+		int y;
+		int w;
+		int h;
+		*/
+		/**
+		@brief affiche l'image et la fenetre en SDL2 
+		@param none
+		**/
+		//void sdlAffichage();
+		/**
+		@brief initialise la fenetre et l'image
+		@param none
+		**/
+		//void afficherInit();
+		/**
+		@brief fait la boucle pour les evenement
+		@param none
+		**/
+ 		//void afficherBoucle();
+
+ 		/**
+ 		@brief fabrique la texture et la surface
+		@param filename : char; renderer : SDL_Renderer
+		**/
+ 		//void afficherCreerImage(const char* filename, SDL_Renderer * renderer);
+
+ 		/**
+		@brief destructeur SDL
+		@param none
+		**/
+ 		//void afficherDestruction();	
+
+public:
+
+	Plateau();
+
+	/**
+ 	@brief Créer le plateau 
+	@param dimx,dimy,nbJoueur : unsigned int
+	**/
+	Plateau(const unsigned int dimx, const unsigned int dimy, const unsigned int nbJoueur);
+
+	~Plateau();
+
+
 
 };
 
