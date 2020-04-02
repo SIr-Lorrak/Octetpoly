6bb7c89a23dab44760acbbe2fb1d96887b16902c
Sebastien Hoareau <sebastien.hoareau@etu.univ-lyon1.fr>
2020-04-02 14:51:48 +0200

    Base de la SDL

--- src/SDL/JeuSDL.h
+++ src/SDL/JeuSDL.h
@@ -1,10 +1,65 @@
 #ifndef OCTET_JEUSDL_H
 #define OCTET_JEUSDL_H
 
-class JeuSDL{
-	public :
 
-	private :
+#include <string>
+#include "SDL.h"
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "Jeu.h"
+
+
+using namespace std;
+
+
+
+class Image{
+
+private:
+
+	SDL_Surface * surface;
+    SDL_Texture * texture;
+
+
+
+public:
+	Image();
+	void loadFichier(const char* nom_fichier, SDL_Renderer * renderer);
+	void dessineTexture(SDL_Renderer * renderer,int x, int y, int w, int h);
+	SDL_Texture * getTexture();
+
+
+};
+
+
+
+
+class JeuSDL{
+
+private:
+	SDL_Window * window;
+	SDL_Renderer * renderer;
+
+
+	Image plateau;
+	Image test;
+
+	bool a;
+
+
+	
+
+public:
+	JeuSDL();
+	~JeuSDL();
+	void affichageJeu();
+	void boucleJeu();
+
+
+		
+		
+
+
 
 };
 
